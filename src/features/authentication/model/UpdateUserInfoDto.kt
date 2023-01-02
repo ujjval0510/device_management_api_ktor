7cfd33df7f3003edc1493027f6216755422ce6ea
Boopalan <boopalan457@gmail.com>
2023-01-02 23:14:47 +0900

    code refactor for UpdateUserInfoDto

--- src/features/authentication/model/UpdateUserInfoDto.kt
+++ src/features/authentication/model/UpdateUserInfoDto.kt
@@ -1,11 +1,7 @@
 package com.neci.features.authentication.model
 
-data class UserInfoDto(
+data class UpdateUserInfoDto(
     val id: Int? = null,
-    val employeeId: String,
-    val role: Int,
-    val email: String,
-    var password: String? = null,
     val firstName: String,
     val lastName: String,
     val designation: String,
